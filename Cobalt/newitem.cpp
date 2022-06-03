ef445b7ce238830e72bf9c38d07b5120b3b36908
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-06-03 13:50:14 +0200

    Ajout du support visual studio code

--- Cobalt/newitem.cpp
+++ Cobalt/newitem.cpp
@@ -8,31 +8,28 @@ enum eItemType {
     BLOCK
 };
 
-newItem::newItem(QWidget *parent) :
+newItem::newItem(QWidget* parent) :
     QMainWindow(parent),
-    ui(new Ui::newItem)
-{
+    ui(new Ui::newItem) {
     ui->setupUi(this);
 
     ui->nameLineEdit->setText("");
 
     ui->subTypeComboBox->setEnabled(false);
 
-    ui->typeComboBox->addItems({"any", "tools", "armor", "block"});
+    ui->typeComboBox->addItems({ "any", "tools", "armor", "block" });
 }
 
-newItem::~newItem()
-{
+newItem::~newItem() {
     delete ui;
 }
 
-void newItem::on_B_clicked()
-{
+void newItem::on_B_clicked() {
     if (!ui->iDLineEdit->text().contains(":")) {
-        QMessageBox *error = new QMessageBox(QMessageBox::Warning, "Invalid ID", "Please include the mod id in the item id (mod_id:item_id).");
+        QMessageBox* error = new QMessageBox(QMessageBox::Warning, "Invalid ID", "Please include the mod id in the item id (mod_id:item_id).");
         error->open();
     } else if (ui->nameLineEdit->text().isEmpty()) {
-        QMessageBox *error = new QMessageBox(QMessageBox::Warning, "Invalid Name", "Please give a name to your item!");
+        QMessageBox* error = new QMessageBox(QMessageBox::Warning, "Invalid Name", "Please give a name to your item!");
         error->open();
     } else {
         QList<QString> lore;
@@ -43,24 +40,23 @@ void newItem::on_B_clicked()
 
 }
 
-void newItem::on_typeComboBox_currentIndexChanged(int index)
-{
+void newItem::on_typeComboBox_currentIndexChanged(int index) {
     ui->subTypeComboBox->clear();
 
     eItemType type = static_cast<eItemType>(index);
-    switch(type) {
+    switch (type) {
     case ANY:
         ui->subTypeComboBox->setEnabled(false);
         break;
 
     case TOOL:
         ui->subTypeComboBox->setEnabled(true);
-        ui->subTypeComboBox->addItems({"axe", "hoe", "pickaxe", "shovel", "sword"});
+        ui->subTypeComboBox->addItems({ "axe", "hoe", "pickaxe", "shovel", "sword" });
         break;
 
     case ARMOR:
         ui->subTypeComboBox->setEnabled(true);
-        ui->subTypeComboBox->addItems({"head", "chest", "legs", "feet"});
+        ui->subTypeComboBox->addItems({ "head", "chest", "legs", "feet" });
         break;
 
     case BLOCK:
